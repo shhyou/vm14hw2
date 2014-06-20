81cfbd40d1634f7be60f4c99dda529d70b35ddee
suhorng <suhorng[at]gmail[dot]com>
2014-06-20 18:22:10 +0800

    add trace event: `kvm_userspace_exit`

--- fs/host_utils/trace_on.sh
+++ fs/host_utils/trace_on.sh
@@ -9,3 +9,4 @@ echo 1 > /sys/kernel/debug/tracing/events/kvm/kvm_hvc/enable
 echo 1 > /sys/kernel/debug/tracing/events/kvm/kvm_entry/enable
 echo 1 > /sys/kernel/debug/tracing/events/kvm/kvm_exit/enable
 echo 1 > /sys/kernel/debug/tracing/events/kvm/kvm_guest_fault/enable
+echo 1 > /sys/kernel/debug/tracing/events/kvm/kvm_userspace_exit/enable
